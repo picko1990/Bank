d248b15db3c30ad194f692c96a34ccd7ed9b1c49
Nikhil Taneja <taneja.nikhil03@gmail.com>
2021-01-16 05:32:40 +0530

    added model to admin

--- v1/tnb_faucet/admin.py
+++ v1/tnb_faucet/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models.tnb_faucet import FaucetModel, FaucetOption
+from .models.tnb_faucet import FaucetModel, FaucetOption, PostModel
 
 admin.site.register(FaucetModel)
 admin.site.register(FaucetOption)
+admin.site.register(PostModel)
