e50c44d6ba4d82c02865f8082331b76db09ca09e
Nikhil Taneja <taneja.nikhil03@gmail.com>
2021-01-16 13:30:02 +0530

    relationship should be one to many

--- v1/tnb_faucet/views/tnb_faucet.py
+++ v1/tnb_faucet/views/tnb_faucet.py
@@ -33,16 +33,16 @@ def faucet_view(request):
             url = urlparse(url_str)
             post = None
             if ((url.netloc == 'www.facebook.com')
-            or (url.netloc == 'www.fb.com')
-            or (url.netloc == 'facebook.com')
-            or (url.netloc == 'fb.com')
-            or (url.netloc == 'm.facebook.com')
-            or (url.netloc == 'mbasic.facebook.com')):
+                or (url.netloc == 'www.fb.com')
+                or (url.netloc == 'facebook.com')
+                or (url.netloc == 'fb.com')
+                or (url.netloc == 'm.facebook.com')
+                    or (url.netloc == 'mbasic.facebook.com')):
                 post = fb_post.process(url_str)
             elif ((url.netloc == 'twitter.com')
-            or (url.netloc == 'www.twitter.com')
-            or (url.netloc == 'mobile.twitter.com')
-            or (url.netloc == 'm.twitter.com')):
+                  or (url.netloc == 'www.twitter.com')
+                  or (url.netloc == 'mobile.twitter.com')
+                  or (url.netloc == 'm.twitter.com')):
                 post = tw_post.process(url_str)
             else:
                 messages.error(
@@ -51,12 +51,12 @@ def faucet_view(request):
 
             if post:
                 receiver_account_number = post.get_account_number()
-                post_id  = post.get_id()
+                post_id = post.get_id()
                 platform = post.get_platform()
-                user_id  = post.get_user()
+                user_id = post.get_user()
 
                 bank_config = SelfConfiguration.objects.first()
-                pv_config   = bank_config.primary_validator
+                pv_config = bank_config.primary_validator
 
                 signing_key = get_signing_key()
                 sender_account_number = encode_verify_key(
@@ -64,24 +64,17 @@ def faucet_view(request):
 
                 account, created = Account.objects.get_or_create(
                     account_number=receiver_account_number,
-                    defaults={'trust': 0},
-                )
+                    defaults={'trust': 0})
 
-                faucet_model = FaucetModel.objects.filter(
-                    Q(post__post_id=post_id) | 
-                    (
-                        (Q(account=account) | Q(social_user_id=user_id)) &
-                        Q(next_valid_access_time__gt=timezone.now())
-                    )
-                ).first()
+                post_model = PostModel.objects.filter(post_id=post_id).first()
+                faucet_model = None
+                if not post_model:
+                    faucet_model = FaucetModel.objects.filter(
+                        (Q(account=account) | Q(social_user_id=user_id)),
+                        next_valid_access_time__gt=timezone.now()
+                    ).latest('next_valid_access_time')
 
-                if not faucet_model:
-                    post_model, created = PostModel.objects.get_or_create(
-                        post_id=post_id,
-                        reward=amount,
-                        social_type=platform
-                    )
-
+                if not post_model or not faucet_model:
                     response = requests.get((
                         f'{pv_config.protocol}://{pv_config.ip_address}'
                         f':{pv_config.port}'f'/accounts/'
@@ -94,14 +87,21 @@ def faucet_view(request):
 
                         faucet_model, created = (
                             FaucetModel.objects.update_or_create(
-                            account=account,
-                            social_user_id=user_id,
-                            defaults={
-                                'next_valid_access_time': (timezone.now()
-                                            + timedelta(hours=amount.delay))
-                            }
-                        ))
-                        faucet_model.post.add(post_model)
+                                account=account,
+                                social_user_id=user_id,
+                                social_type=platform,
+                                defaults={
+                                    'next_valid_access_time': (
+                                        timezone.now()
+                                        + timedelta(hours=amount.delay))
+                                })
+                        )
+
+                        post_model, created = PostModel.objects.get_or_create(
+                            post_id=post_id,
+                            reward=amount,
+                            social_user=faucet_model
+                        )
 
                         transactions = [
                             {
@@ -117,6 +117,7 @@ def faucet_view(request):
                                 'recipient': pv_config.account_number,
                             }
                         ]
+
                         block = generate_block(
                             account_number=signing_key.verify_key,
                             balance_lock=balance_lock,
@@ -132,35 +133,46 @@ def faucet_view(request):
                         messages.success(
                             request,
                             (f'SUCCESS! {amount.coins} faucet funds'
-                            f' transferred to {receiver_account_number}.'))
+                             f' transferred to {receiver_account_number}.'))
                         form = None
                     else:
-                        messages.error(request, 'Unable to obtain TNB account details!')
+                        messages.error(
+                            request,
+                            'Unable to obtain TNB account details!'
+                        )
                 else:
                     form = None
-                    duration = faucet_model.next_valid_access_time - timezone.now()
+                    duration = (faucet_model.next_valid_access_time
+                                - timezone.now())
                     totsec = duration.total_seconds()
                     if totsec > 0:
-                        h = int(totsec//3600)
-                        m = int((totsec%3600) // 60)
-                        sec = round((totsec%3600) % 60)
+                        h = int(totsec // 3600)
+                        m = int((totsec % 3600) // 60)
+                        sec = round((totsec % 3600) % 60)
                         messages.error(
                             request,
                             ('Slow down! Try again after ('
-                            f'{h} hours {m} mins and {sec} secs'
-                            ') till cooldown period expires.'))
+                             f'{h} hours {m} mins and {sec} secs'
+                             ') till cooldown period expires.')
+                        )
                     else:
                         messages.error(
                             request,
-                            ('Smarty! Same post cannot be used again. Try again with a new one :P'))
+                            ('Same post cannot be used again. '
+                             ' Try again with a new one :P')
+                        )
             else:
                 messages.error(
                     request,
                     ('Failed to extract information!'
-                    ' Make sure post is public,'
-                    ' contains #TNBFaucet and your account number'))
+                     ' Make sure post is public,'
+                     ' contains #TNBFaucet and your account number')
+                )
         else:
-            messages.error(request, 'Form invalid! Please provide correct details!')
+            messages.error(
+                request,
+                'Form invalid! Please provide correct details!'
+            )
 
     context = {
         'form': form
