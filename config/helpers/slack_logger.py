c5639eca864c3db10f2f67edf316d727c6494eef
Nikhil Taneja <taneja.nikhil03@gmail.com>
2021-01-17 23:53:10 +0530

    basic alerts

--- config/helpers/slack_logger.py
+++ config/helpers/slack_logger.py
@@ -6,7 +6,7 @@ from copy import copy
 import requests
 from django.conf import settings
 from django.utils.log import AdminEmailHandler
-from django.views.debug import ExceptionReporter
+# from django.views.debug import ExceptionReporter
 
 
 class SlackExceptionHandler(AdminEmailHandler):
@@ -37,13 +37,13 @@ class SlackExceptionHandler(AdminEmailHandler):
         no_exc_record.exc_info = None
         no_exc_record.exc_text = None
 
-        if record.exc_info:
-            exc_info = record.exc_info
-        else:
-            exc_info = (None, record.getMessage(), None)
+        # if record.exc_info:
+        #     exc_info = record.exc_info
+        # else:
+        #     exc_info = (None, record.getMessage(), None)
 
-        reporter = ExceptionReporter(request, is_email=True, *exc_info)
-        message = reporter.get_traceback_data()
+        # reporter = ExceptionReporter(request, is_email=True, *exc_info)
+        # message = reporter.get_traceback_data()
         # message = '%s\n\n%s' % (self.format(no_exc_record), reporter.get_traceback_text())
 
         # self.send_mail(subject, message, fail_silently=True, html_message=html_message)
@@ -51,106 +51,108 @@ class SlackExceptionHandler(AdminEmailHandler):
         # this is where original 'emit' method code ends
 
         # construct slack attachment detail fields
-        colors = {
-            'ERROR': 'danger',
-            'INFO': 'good',
-            'WARNING': 'warning'
-        }
-        attachments = [
-            {
-                'title': subject,
-                'color': colors.get(record.levelname, '#1d9bd1'),
-                'fields': [
-                    {
-                        'title': 'Level',
-                        'value': record.levelname,
-                        'short': True,
-                    },
-                    {
-                        'title': 'Method',
-                        'value': request.method if request else 'No Request',
-                        'short': True,
-                    },
-                    {
-                        'title': 'Path',
-                        'value': request.path if request else 'No Request',
-                        'short': True,
-                    },
-                    {
-                        'title': 'User',
-                        'value': ((request.user.username + ' (' + str(request.user.pk) + ')'
-                                   if request.user.is_authenticated else 'Anonymous')
-                                  if request else 'No Request'),
-                        'short': True,
-                    },
-                    {
-                        'title': 'Status Code',
-                        'value': getattr(record, 'status_code', None),
-                        'short': True,
-                    },
-                    {
-                        'title': 'UA',
-                        'value': (request.META['HTTP_USER_AGENT']
-                                  if request and request.META else 'No Request'),
-                        'short': False,
-                    },
-                    {
-                        'title': 'GET Params',
-                        'value': json.dumps(request.GET) if request else 'No Request',
-                        'short': False,
-                    }
-                ],
+        # colors = {
+        #     'ERROR': 'danger',
+        #     'INFO': 'good',
+        #     'WARNING': 'warning'
+        # }
+        # attachments = [
+        #     {
+        #         'title': subject,
+        #         'color': colors.get(record.levelname, '#1d9bd1'),
+        #         'fields': [
+        #             {
+        #                 'title': 'Level',
+        #                 'value': record.levelname,
+        #                 'short': True,
+        #             },
+        #             {
+        #                 'title': 'Method',
+        #                 'value': request.method if request else 'No Request',
+        #                 'short': True,
+        #             },
+        #             {
+        #                 'title': 'Path',
+        #                 'value': request.path if request else 'No Request',
+        #                 'short': True,
+        #             },
+        #             {
+        #                 'title': 'User',
+        #                 'value': ((request.user.username + ' (' + str(request.user.pk) + ')'
+        #                            if request.user.is_authenticated else 'Anonymous')
+        #                           if request else 'No Request'),
+        #                 'short': True,
+        #             },
+        #             {
+        #                 'title': 'Status Code',
+        #                 'value': getattr(record, 'status_code', None),
+        #                 'short': True,
+        #             },
+        #             {
+        #                 'title': 'UA',
+        #                 'value': (request.META['HTTP_USER_AGENT']
+        #                           if request and request.META else 'No Request'),
+        #                 'short': False,
+        #             },
+        #             {
+        #                 'title': 'GET Params',
+        #                 'value': json.dumps(request.GET) if request else 'No Request',
+        #                 'short': False,
+        #             }
+        #         ],
             },
 
         ]
-        if record.levelname == 'ERROR':
-            extra_data = [
-                'frames',
-                'request_meta',
-                'filtered_POST_items',
-                'template_info',
-                'template_does_not_exist',
-                'postmortem'
-            ]
+        # if record.levelname == 'ERROR':
+        #     extra_data = [
+        #         'frames',
+        #         'request_meta',
+        #         'filtered_POST_items',
+        #         'template_info',
+        #         'template_does_not_exist',
+        #         'postmortem'
+        #     ]
 
-            # slack message attachment text has max of 8000 bytes
-            # lets split it up into 7900 bytes long chunks to be on the safe side
-            split = 7900
-            byte_size = 0
-            response = ''
-            part = 1
+        #     slack message attachment text has max of 8000 bytes
+        #     lets split it up into 7900 bytes long chunks to be on the safe side
+        #     split = 7900
+        #     byte_size = 0
+        #     response = ''
+        #     part = 1
 
-            for field in extra_data:
-                data = json.dumps(message[field], indent=2, default=str)
-                byte_size += len(data) + len(field) + len(data) + 3
-                if byte_size < split:
-                    response += f'{field}:\n{data}\n'
-                else:
-                    # add main error message body
-                    attachments.append({
-                        'color': 'danger',
-                        'title': f'Extra details ({part})',
-                        'text': response,
-                        'ts': time.time(),
-                    })
+        #     for field in extra_data:
+        #         data = json.dumps(message[field], indent=2, default=str)
+        #         byte_size += len(data) + len(field) + len(data) + 3
+        #         if byte_size < split:
+        #             response += f'{field}:\n{data}\n'
+        #         else:
+        #             # add main error message body
+        #             attachments.append({
+        #                 'color': 'danger',
+        #                 'title': f'Extra details ({part})',
+        #                 'text': response,
+        #                 'ts': time.time(),
+        #             })
 
-                    part += 1
-                    byte_size = len(data) + len(field) + len(data) + 3
-                    response = f'{field}:\n{data}\n'
-            # add main error message body
-            attachments.append({
-                'color': 'danger',
-                'title': f'Extra details ({part})',
-                'text': response,
-                'ts': time.time(),
-            })
+        #             part += 1
+        #             byte_size = len(data) + len(field) + len(data) + 3
+        #             response = f'{field}:\n{data}\n'
+        #     # add main error message body
+        #     attachments.append({
+        #         'color': 'danger',
+        #         'title': f'Extra details ({part})',
+        #         'text': response,
+        #         'ts': time.time(),
+        #     })
 
         # construct main text
-        main_text = 'Alert at ' + time.strftime('%A, %d %b %Y %H:%M:%S +0000', time.gmtime())
+        # main_text = 'Alert at ' + time.strftime('%A, %d %b %Y %H:%M:%S +0000', time.gmtime())
 
         # construct data
         data = {
-            'payload': json.dumps({'main_text': main_text, 'attachments': attachments}),
+            'payload': json.dumps({
+                'text': f'*{record.levelname}:* {subject}',
+                'unfurl_links': True}),
         }
 
         # setup channel webhook
