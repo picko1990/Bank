bb7ef30975f988903b4cd43938ab1a89593092bc
abdelmalek tag <tagabdelmalek1999@gmail.com>
2021-07-22 00:06:46 +0100

    added api support

--- v1/tnb_faucet/serializers/tnb_faucet.py
+++ v1/tnb_faucet/serializers/tnb_faucet.py
@@ -1,7 +1,8 @@
-# from rest_framework import serializers
 # from thenewboston.utils.fields import all_field_names
 
 # from ..models.transaction_log import TransactionLog
+from rest_framework import serializers
+from ..models.tnb_faucet import FaucetOption
 
 
 # class TransactionLogSerializer(serializers.ModelSerializer):
@@ -17,3 +18,19 @@
 #         model = TransactionLog
 #         fields = '__all__'
 #         read_only_fields = all_field_names(TransactionLog)
+
+
+class FaucetOptionSerializer(serializers.ModelSerializer):
+    string = serializers.SerializerMethodField()
+
+    class Meta:
+        model = FaucetOption
+        fields = ['id', 'string']
+
+    def get_string(self, obj):
+        return obj.__str__()
+
+
+class FormSerializer(serializers.Serializer):
+    url = serializers.URLField()
+    faucet_option_id = serializers.IntegerField()
--- v1/tnb_faucet/urls.py
+++ v1/tnb_faucet/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from .views.tnb_faucet import faucet_view
+from .views.tnb_faucet import faucet_view, API
 
 urlpatterns = [
-    path('', faucet_view, name='faucet')
+    path('', faucet_view, name='faucet'),
+    path('api', API.as_view(), name='api'),
 ]
