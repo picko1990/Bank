03b736c121f28b308ecd10b96f0206ebb3b04f8a
Nikhil Taneja <taneja.nikhil03@gmail.com>
2021-08-17 01:44:15 +0530

    Update tnb_faucet.py

--- v1/tnb_faucet/views/tnb_faucet.py
+++ v1/tnb_faucet/views/tnb_faucet.py
@@ -162,14 +162,17 @@ def faucet_view(request):
                                 {
                                     'amount': amount.coins,
                                     'recipient': receiver_account_number,
+                                    memo: "Thank you for using TNBExplorer testnet"
                                 },
                                 {
                                     'amount': bank_config.default_transaction_fee,
                                     'recipient': bank_config.account_number,
+                                    fee: "BANK"
                                 },
                                 {
                                     'amount': pv_config.default_transaction_fee,
                                     'recipient': pv_config.account_number,
+                                    fee: "PRIMARY_VALIDATOR"
                                 }
                             ]
 
@@ -309,14 +312,17 @@ class API(APIView):
                                     {
                                         'amount': amount.coins,
                                         'recipient': receiver_account_number,
+                                        memo: "Thank you for using TNBExplorer testnet"
                                     },
                                     {
                                         'amount': bank_config.default_transaction_fee,
                                         'recipient': bank_config.account_number,
+                                        fee: "BANK"
                                     },
                                     {
                                         'amount': pv_config.default_transaction_fee,
                                         'recipient': pv_config.account_number,
+                                        fee: "PRIMARY_VALIDATOR"
                                     }
                                 ]
 
